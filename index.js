6fe91d1b2d5cb2553ccb19ba4919748482d2be09
masum691 <masumhu691@gmail.com>
2022-01-20 16:59:03 +0600

    delete api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb');
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const port = process.env.PORT || 5000
 // middlewre 
@@ -16,12 +17,29 @@ async function run() {
         await client.connect();
         const database = client.db("todoDB");
         const todoCollection = database.collection("todo_planner");
+
+        // get api
+        app.get('/mytodo', async(req, res) => {
+            const cursor = todoCollection.find({})
+            const todo = await cursor.toArray();
+            res.send(todo);
+        })
+
         // post api
         app.post('/mytodo', async(req, res) => {
             const newList = req.body;
-            console.log(newList)
+            // console.log(newList)
             const result = await todoCollection.insertOne(newList)
-            console.log(result)
+            // console.log(result)
+            res.json(result)
+        })
+
+        // delete api
+        app.delete('/mytodo/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await todoCollection.deleteOne(query);
+            // console.log('deleteing', result)
             res.json(result)
         })
 
