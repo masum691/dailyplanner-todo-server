5eb8b09d0925b47b47ef9f44fce48bca5acb2e9b
masum691 <masumhu691@gmail.com>
2022-01-20 13:03:14 +0600

    bug fixed

--- index.js
+++ index.js
@@ -4,26 +4,38 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
-
-
 // middlewre 
 app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ep562.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+console.log(uri)
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("todoDB");
+        const todoCollection = database.collection("todo_planner");
+        // post api
+        app.post('/mytodo', async(req, res) => {
+            const newList = req.body;
+            console.log(newList)
+            const result = await todoCollection.insertOne(newList)
+            console.log(result)
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
